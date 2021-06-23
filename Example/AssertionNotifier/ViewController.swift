2fd8f340125395395e8dfc4814e4e3699504b075
Elísio Fernandes <leak.bronco_0g@icloud.com>
2021-06-23 08:21:12 +0100

    Initial commit

---
+++ Example/AssertionNotifier/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AssertionNotifier
+//
+//  Created by Elísio Fernandes on 06/23/2021.
+//  Copyright (c) 2021 Elísio Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
